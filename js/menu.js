5b3839cb4d63169b0f7fc5791062db98889f5d45
Jaime <jaime.mena8@gmail.com>
2020-05-21 18:03:01 +0200

    More changes

--- js/menu.js
+++ js/menu.js
@@ -1,31 +1,31 @@
 //Menú
-$(".menuColapsado").click(function() {
-  $(this).toggleClass("menuExpandido");
-});
+// $(".menuColapsado").click(function() {
+//   $(this).toggleClass("menuExpandido");
+// });
 
 
 
 //para que suba despacio al dar a "Subir"
-$("#btnUP").click(function() {
-    $('html, body').animate({
-        scrollTop: $(".wrapper").offset().top
-    }, 1500);
-});
+// $("#btnUP").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $(".wrapper").offset().top
+//     }, 1500);
+// });
 
-$(document).on("scroll", function(){
-    //sacamos el desplazamiento actual de la página
-    var desplazamientoActual = $(document).scrollTop();
-    //accedemos al control de "ir arriba"
-    var controlArriba = $("#btnUP");
-    //compruebo si debo mostrar el botón
-    if(desplazamientoActual > 300 && controlArriba.css("display") == "none"){
-        controlArriba.fadeIn(800);
-    }
-    //controlo si debo ocultar el botón
-    if(desplazamientoActual < 300 && controlArriba.css("display") == "block"){
-        controlArriba.fadeOut(800);
-    }
-});
+// $(document).on("scroll", function(){
+//     //sacamos el desplazamiento actual de la página
+//     var desplazamientoActual = $(document).scrollTop();
+//     //accedemos al control de "ir arriba"
+//     var controlArriba = $("#btnUP");
+//     //compruebo si debo mostrar el botón
+//     if(desplazamientoActual > 300 && controlArriba.css("display") == "none"){
+//         controlArriba.fadeIn(800);
+//     }
+//     //controlo si debo ocultar el botón
+//     if(desplazamientoActual < 300 && controlArriba.css("display") == "block"){
+//         controlArriba.fadeOut(800);
+//     }
+// });
 
 
 
